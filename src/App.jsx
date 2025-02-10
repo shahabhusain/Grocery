c176e620125bb0e82c9c8925130b13e938a396fb
shahabhusain <shahabhussain098123@gmail.com>
2025-02-10 13:19:07 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './router/router'
+import { router } from './router/Router'
 
 const App = () => {
   return (
